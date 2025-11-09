fb6a874a45a7c6e9dfef041ece307a7796469ef9
Peniel Negasa <penaye779@gmail.com>
2025-11-09 21:02:26 +0300

    Fix: use exact calculation seconds = hours * 3600 for checker

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,11 +1,3 @@
-# hours_to_seconds.py
-
-# Step 1: Define hours
 hours = 2
-
-# Step 2: Convert hours to seconds
-seconds_in_one_hour = 3600
-seconds = hours * seconds_in_one_hour
-
-# Step 3: Print the result
+seconds = hours * 3600
 print(hours, "hour(s) is", seconds, "seconds.")
